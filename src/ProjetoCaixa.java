3da9d0d7caf4469e46900e04101f0ca40e123e1d
EDILILSON DAVID MATIAS DA SILVA <edililsond@gmail.com>
2020-07-02 18:19:30 -0300

    Update inicial do projeto

---
+++ src/ProjetoCaixa.java
@@ -0,0 +1,14 @@
+
+public class ProjetoCaixa {
+
+	public static void main(String[] args) {
+		
+		criarConta();
+		
+	}
+	
+	public static void criarConta() {
+		System.out.println("Bem vindo ao projeto Start Latam!");
+	}
+
+}
